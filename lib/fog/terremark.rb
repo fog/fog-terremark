148554d4c36480ee06b4a929c34c7624ed72ab6d
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-11-13 18:01:50 -0200

    Initial Commit

---
+++ lib/fog/terremark.rb
@@ -0,0 +1,7 @@
+require "fog/terremark/version"
+
+module Fog
+  module Terremark
+    # Your code goes here...
+  end
+end
---
+++ lib/fog/terremark/version.rb
@@ -0,0 +1,5 @@
+module Fog
+  module Terremark
+    VERSION = "0.0.1"
+  end
+end
