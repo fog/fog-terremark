3de9c429128708d260e870ee12211f76032ab10d
Nathan Buesgens <nathan@natb1.com>
2014-11-21 22:06:40 +0000

    tests valid base64 encoding

--- spec/credentials_spec.rb
+++ spec/credentials_spec.rb
@@ -1,4 +1,7 @@
 require 'minitest_helper'
+require 'base64'
+
+mock_excon = true
 
 mock_org_list = '<OrgList xmlns="http://www.vmware.com/vCloud/v0.8"
 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -7,12 +10,25 @@ xmlns:xsd="http://www.w3.org/2001/XMLSchema">
 type="application/vnd.vmware.vCloud.org+xml" name="Org Name"/>
 </OrgList>'
 
+test_user = 'testtesttesttesttesttest@example.com'
+test_password = '1234567890testtesttest1234567890123456'
+test_authstring = "Basic #{Base64.strict_encode64("#{test_user}:#{test_password}")}"
+
 describe Fog::Terremark::Vcloud do
 
   before do
-    Excon.defaults[:mock] = true
+    Excon.defaults[:mock] = mock_excon
     Excon.stub(
-      {:method => :post, :path => '/api/v0.8a-ext1.6/login'},
+      {
+        :method => :post,
+        :path => '/api/v0.8a-ext1.6/login',
+        :headers => {
+          'Authorization' => test_authstring,
+          'Content-Type' => 'application/vnd.vmware.vcloud.orgList+xml',
+          'Host' => 'services.vcloudexpress.terremark.com:443',
+          'User-Agent' => 'fog-core/1.25.0'
+        }
+      },
       {
         :status => 200,
         :headers => {
@@ -25,8 +41,8 @@ describe Fog::Terremark::Vcloud do
 
   it 'gets a login token' do
     service = Fog::Terremark::Vcloud.new(
-        :terremark_vcloud_username => 'test@example.com',
-        :terremark_vcloud_password => '123456')
+        :terremark_vcloud_username => test_user,
+        :terremark_vcloud_password => test_password)
     service.send(:auth_token)
   end
 end
