1deda2044f1ff61046d84bf2aaa94e746d94eac1
Nathan Buesgens <nathan@natb1.com>
2014-11-21 21:03:53 +0000

    mocks the excon response

--- spec/credentials_spec.rb
+++ spec/credentials_spec.rb
@@ -1,9 +1,28 @@
 require 'minitest_helper'
 
+mock_org_list = '<OrgList xmlns="http://www.vmware.com/vCloud/v0.8"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+<Org href="https://services.vCloudexpress.terremark.com/api/v0.8a-ext1.6/org/32"
+type="application/vnd.vmware.vCloud.org+xml" name="Org Name"/>
+</OrgList>'
+
 describe Fog::Terremark::Vcloud do
+
   before do
-    
+    Excon.defaults[:mock] = true
+    Excon.stub(
+      {:method => :post, :path => '/api/v0.8a-ext1.6/login'},
+      {
+        :status => 200,
+        :headers => {
+          'Set-Cookie' => 'test-cookie',
+        },
+        :body => mock_org_list
+      }
+    )
   end
+
   it 'gets a login token' do
     service = Fog::Terremark::Vcloud.new(
         :terremark_vcloud_username => 'test@example.com',
