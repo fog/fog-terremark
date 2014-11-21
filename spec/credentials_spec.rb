2dccbef72edfb7724bed049c30973af780b89da4
Nathan Buesgens <nathan@natb1.com>
2014-11-21 20:39:25 +0000

    uses the vcloud service that has the required utility methods

--- spec/credentials_spec.rb
+++ spec/credentials_spec.rb
@@ -1,9 +1,13 @@
 require 'minitest_helper'
 
-describe Fog::Compute::Terremark do
-  it 'authenticates' do
-    Fog::Compute::Terremark.new(
+describe Fog::Terremark::Vcloud do
+  before do
+    
+  end
+  it 'gets a login token' do
+    service = Fog::Terremark::Vcloud.new(
         :terremark_vcloud_username => 'test@example.com',
         :terremark_vcloud_password => '123456')
+    service.send(:auth_token)
   end
 end
